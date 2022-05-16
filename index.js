92bded4e8edf1cf0ba1f7bcbefef5bb52b2f0794
Baggsterhd <73445473+Baggsterhd@users.noreply.github.com>
2022-05-16 12:46:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/about.html")
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/music.html")
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/swedish.html")
+});
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
