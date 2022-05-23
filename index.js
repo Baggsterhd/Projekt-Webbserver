c4b78811103c0608f93750b830abf626d08ac5b4
Baggsterhd <73445473+Baggsterhd@users.noreply.github.com>
2022-05-23 11:22:39 +0200

    dasda

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+const expressServer = http.createServer(app);
+
 const express = require('express')
 const app = express();
 const port = 3000;
@@ -43,3 +49,35 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/swedish.html');
 });
 
+
+
+
+
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const io = new Server(expressServer);
+
+io.on('connect', function (socket) {
+    console.log('a user is connected');
+
+    setInterval(function () {
+        let date = new Date().toLocaleTimeString()
+        socket.send(date)
+    }, 1000)
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected.')
+    })
+
+})
+
+app.get('/', (req, res, next) => {
+    res.render('index.html');
+})
+
+expressServer.listen(4000, () => {
+    console.log('server is listening.')
+})
